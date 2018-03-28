54cc0664a5ce284ddc501045778e885721770a29
14104876 <14104876@w1975218.portoalegre.pucrsnet.br>
2018-03-28 18:20:15 -0300

    Começando o projeto.

---
+++ src/Irpf.java
@@ -0,0 +1,42 @@
+
+public class Irpf {
+private String nome;
+private String cpf;
+public Irpf(String nome,String cpf) {
+	this.nome=nome;
+	this.cpf=cpf;
+}
+public double getTotRendimentos() {
+	return 0;
+}
+public void setTotRendimentos(double totRendimentos) {
+	
+}
+public double getContrPrev() {
+	return 0;
+}
+public void setContrPrev(double contrPrev) {
+	
+}
+public int getNroDep() {
+	return 0;
+}
+public void setNroDep(int nroDep) {
+	
+}
+public int getIdade() {
+	return 0;
+}
+public void setIdade(int idade) {
+	
+}
+public String getNome() {
+	return nome;
+}
+public String getCpf() {
+	return cpf;
+}
+public double impostoDevido() {
+	return 0;
+}
+}
